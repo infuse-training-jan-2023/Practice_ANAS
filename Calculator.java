0bd3f4fb29ddc4fbaf7754f78804f29ec0f34368
AnisShaikh10 <anisofficial0208@gmail.com>
2023-01-22 17:23:00 +0530

    added subtraction function

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,17 @@
 class Calculator
 {
+    public void subtraction(int a, int b)
+    {
+        int result = a - b;
+        System.out.println("Subtraction Result is " + result);
+    }
 
     public static void main(String[] args)
     {
         System.out.println("Hello World");
+        Calculator calc = new Calculator();
+        int a= 10, b=5;
+        calc.subtraction(a, b);
     }
+
 }
