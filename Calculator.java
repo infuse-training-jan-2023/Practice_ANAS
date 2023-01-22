60092b697ee515e63cde84fa545beebf14fc9474
AnisShaikh10 <anisofficial0208@gmail.com>
2023-01-22 16:42:10 +0530

    added multiplication function

--- Calculator.java
+++ Calculator.java
@@ -6,10 +6,18 @@ class Calculator
         System.out.println("Subtraction Result is " + result);
     }
 
+    public void multiplication(int a, int b)
+    {
+        int result = a * b;
+        System.out.println("Multiplication Result is " + result);
+    }
+
     public static void main(String[] args)
     {
         System.out.println("Hello World");
         Calculator calc = new Calculator();
-        calc.subtraction(10, 5);
+        int a=10, b=5;
+        calc.subtraction(a, b);
+        calc.multiplication(a, b);
     }
 }
